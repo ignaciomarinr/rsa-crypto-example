938203e2e3196ed118ae5751f065f13825f76497
Valen Blanco <valentin_gomez1@hotmail.com>
2017-10-15 18:09:12 +0200

    Prepared for delivering

--- src/RSALibrary.java
+++ src/RSALibrary.java
@@ -1,18 +1,13 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.Security;
 import java.security.Signature;
+
 import javax.crypto.Cipher;
 
 public class RSALibrary {
@@ -26,38 +21,31 @@ public class RSALibrary {
 	public final String PUBLIC_KEY_FILE = "./public.key";
 
 	/***********************************************************************************/
-	/*
-	 * Generates an RSA key pair (a public and a private key) of 1024 bits
-	 * length
-	 */
-	/*
-	 * Stores the keys in the files defined by PUBLIC_KEY_FILE and
-	 * PRIVATE_KEY_FILE
-	 */
-	/* Throws IOException */
+	/* Generates an RSA key pair (a public and a private key) of 1024 bits length. */
+	/* Stores the keys in the files defined by PUBLIC_KEY_FILE and PRIVATE_KEY_FILE. */
+	/* Throws IOException. */
 	/***********************************************************************************/
 	public void generateKeys() throws IOException {
 		try {
 			final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
 			keyGen.initialize(1024);
 
-			// Use KeyGen to generate a public and a private key
+			// Use KeyGen to generate a public and a private key.
 			KeyPair kp = keyGen.generateKeyPair();
-			
-			// Store the public key in the file PUBLIC_KEY_FILE
+
+			// Store the public key in the file PUBLIC_KEY_FILE.
 			PublicKey pk = kp.getPublic();
 			try (FileOutputStream fos = new FileOutputStream(PUBLIC_KEY_FILE);
 					ObjectOutputStream oos = new ObjectOutputStream(fos)) {
 				oos.writeObject(pk);
 			}
 
-			// Store the private key in the file PRIVATE_KEY_FILE
+			// Store the private key in the file PRIVATE_KEY_FILE.
 			PrivateKey sk = kp.getPrivate();
 			try (FileOutputStream fos = new FileOutputStream(PRIVATE_KEY_FILE);
 					ObjectOutputStream oos = new ObjectOutputStream(fos)) {
 				oos.writeObject(sk);
 			}
-
 		} catch (NoSuchAlgorithmException e) {
 			System.out.println("Exception: " + e.getMessage());
 			System.exit(-1);
@@ -66,43 +54,41 @@ public class RSALibrary {
 
 	/***********************************************************************************/
 	/* Encrypts a plaintext using an RSA public key. */
-	/* Arguments: the plaintext and the RSA public key */
-	/* Returns a byte array with the ciphertext */
+	/* Arguments: the plaintext and the RSA public key. */
+	/* Returns a byte array with the ciphertext. */
 	/***********************************************************************************/
 	public byte[] encrypt(byte[] plaintext, PublicKey key) {
 		byte[] ciphertext = null;
 
 		try {
-			// Gets an RSA cipher object
+			// Gets an RSA cipher object.
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 
-			// Initialize the cipher object and use it to encrypt the plaintext
+			// Initialize the cipher object and use it to encrypt the plaintext.
 			cipher.init(Cipher.ENCRYPT_MODE, key);
 			ciphertext = cipher.doFinal(plaintext);
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return ciphertext;
 	}
 
 	/***********************************************************************************/
 	/* Decrypts a ciphertext using an RSA private key. */
-	/* Arguments: the ciphertext and the RSA private key */
-	/* Returns a byte array with the plaintext */
+	/* Arguments: the ciphertext and the RSA private key. */
+	/* Returns a byte array with the plaintext. */
 	/***********************************************************************************/
 	public byte[] decrypt(byte[] ciphertext, PrivateKey key) {
 		byte[] plaintext = null;
 
 		try {
-			// Gets an RSA cipher object
+			// Gets an RSA cipher object.
 			final Cipher cipher = Cipher.getInstance(ALGORITHM);
 
-			// Initialize the cipher object and use it to decrypt the ciphertext
+			// Initialize the cipher object and use it to decrypt the ciphertext.
 			cipher.init(Cipher.DECRYPT_MODE, key);
 			plaintext = cipher.doFinal(ciphertext);
-
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -112,25 +98,24 @@ public class RSALibrary {
 
 	/***********************************************************************************/
 	/* Signs a plaintext using an RSA private key. */
-	/* Arguments: the plaintext and the RSA private key */
-	/* Returns a byte array with the signature */
+	/* Arguments: the plaintext and the RSA private key. */
+	/* Returns a byte array with the signature. */
 	/***********************************************************************************/
 	public byte[] sign(byte[] plaintext, PrivateKey key) {
 		byte[] signedInfo = null;
 
 		try {
-			// Gets a Signature object
+			// Gets a Signature object.
 			Signature signature = Signature.getInstance("SHA1withRSA");
 
-			// Initialize the signature oject with the private key
+			// Initialize the signature object with the private key.
 			signature.initSign(key);
 
-			// Set plaintext as the bytes to be signed
+			// Set plaintext as the bytes to be signed.
 			signature.update(plaintext);
 
-			// Sign the plaintext and obtain the signature (signedInfo)
+			// Sign the plaintext and obtain the signature (signedInfo).
 			signedInfo = signature.sign();
-
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
@@ -140,28 +125,25 @@ public class RSALibrary {
 
 	/***********************************************************************************/
 	/* Verifies a signature over a plaintext */
-	/*
-	 * Arguments: the plaintext, the signature to be verified (signed) /* and
-	 * the RSA public key
-	 */
-	/* Returns TRUE if the signature was verified, false if not */
+    /* Arguments: the plaintext, the signature to be verified (signed) 
+    /* and the RSA public key */
+    /* Returns TRUE if the signature was verified, false if not */
 	/***********************************************************************************/
 	public boolean verify(byte[] plaintext, byte[] signed, PublicKey key) {
 		boolean result = false;
 
 		try {
-			// Gets a Signature object
+			// Gets a Signature object.
 			Signature signature = Signature.getInstance("SHA1withRSA");
 
-			// Initialize the signature oject with the public key
+			// Initialize the signature object with the public key.
 			signature.initVerify(key);
 
-			// Set plaintext as the bytes to be veryfied
+			// Set plaintext as the bytes to be verified.
 			signature.update(plaintext);
 
-			// Verify the signature (signed). Store the outcome in the boolean result
+			// Verify the signature (signed). Store the outcome in the boolean result.
 			result = signature.verify(signed);
-
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
--- src/Test.java
+++ src/Test.java
@@ -9,44 +9,68 @@ import java.security.PublicKey;
 import javax.xml.bind.DatatypeConverter;
 
 public class Test {
+	public static final String inputTextFilename = "in.txt";
+	public static final String skFilename = "private.key";
+	public static final String pkFilename = "public.key";
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		byte[] inputText = Files.readAllBytes(Paths.get("in.txt"));
+		byte[] inputText = Files.readAllBytes(Paths.get(inputTextFilename));
 		RSALibrary rsaLibrary = new RSALibrary();
+
+		System.out.println("Input (hex): " + DatatypeConverter.printHexBinary(inputText));
+		System.out.println("Input size (bytes): " + inputText.length + "\n");
+		
+
+		/* Generation */
+
+		// Generate private and public key files.
 		rsaLibrary.generateKeys();
+		
+
+		/* Encryption */
+
+		// Read the public key.
 		PublicKey pk = null;
-		PrivateKey sk = null;
-		
-		System.out.println("input.txt (hex): " + DatatypeConverter.printHexBinary(inputText));
-		System.out.println("input.txt size (bytes): " + inputText.length);
-		
-		try (FileInputStream fis = new FileInputStream("public.key");
+
+		try (FileInputStream fis = new FileInputStream(pkFilename);
 				ObjectInputStream ois = new ObjectInputStream(fis)) {
 			pk = (PublicKey) ois.readObject();
 		}
-		
+
+		System.out.println("Public key: " + pk + "\n");
+
+		// Encrypt input text (public key).
 		byte[] ciphertext = rsaLibrary.encrypt(inputText, pk);
-		
+
 		System.out.println("Ciphertext (hex): " + DatatypeConverter.printHexBinary(ciphertext));
-		System.out.println("Ciphertext size (bytes): " + ciphertext.length);
-		
-		try (FileInputStream fis = new FileInputStream("private.key");
+		System.out.println("Ciphertext size (bytes): " + ciphertext.length + "\n");
+
+		// Read the private key.
+		PrivateKey sk = null;
+
+		try (FileInputStream fis = new FileInputStream(skFilename);
 				ObjectInputStream ois = new ObjectInputStream(fis)) {
 			sk = (PrivateKey) ois.readObject();
 		}
-		
+
+		//System.out.println("Private key: " + sk. + "\n");
+
+		// Decrypt ciphertext (private key):
 		byte[] plaintext = rsaLibrary.decrypt(ciphertext, sk);
-		
+
 		System.out.println("Plaintext (hex): " + DatatypeConverter.printHexBinary(plaintext));
-		System.out.println("Plaintext size (bytes): " + plaintext.length);
+		System.out.println("Plaintext size (bytes): " + plaintext.length + "\n");
+
 		
-		// FIRMA
-		
-		byte[] signedtext = rsaLibrary.sign(inputText, sk);
-		
-		System.out.println("Signedtext (hex): " + DatatypeConverter.printHexBinary(signedtext));
-		System.out.println("Signedtext size (bytes): " + signedtext.length);
-		
-		
-		System.out.println("Verify sign: " + rsaLibrary.verify(inputText/*new byte[]{0x48,0x4F,0x4C,0x41,0x0A}*/, signedtext, pk));
+		/* Signing */
+
+		// Sign input text (private key).
+		byte[] signedText = rsaLibrary.sign(inputText, sk);
+
+		System.out.println("Signed text (hex): " + DatatypeConverter.printHexBinary(signedText));
+		System.out.println("Signed text size (bytes): " + signedText.length + "\n");
+
+		// Verify the sign (public key).
+		System.out.println("Verify sign: " + rsaLibrary.verify(inputText, signedText, pk));
 	}
 }
